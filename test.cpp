71ee7ccf2666fe0454b28c9784e61b7bce458723
Drew Smith <smithdrew555@yahoo.com>
2022-03-08 14:06:32 -0500

    added hacky fix

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@
 int
 main(int argc, char *argv[])
 {
+    Py_SetPath(PYTHON_LIB);
     wchar_t *program = Py_DecodeLocale(argv[0], NULL);
     if (program == NULL) {
         fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
